aed9b86e7f439b29221c3cf5e67d7c1033e18bc3
lchin <l.chin@ufl.edu>
2024-09-28 18:01:23 -0400

    popup

--- frontend/popup.js
+++ frontend/popup.js
@@ -1,27 +1,24 @@
 document.getElementById('subject').addEventListener('change', function() {
     let selectedElement = document.querySelector('#subject');
     let chosen = selectedElement.value;
-    console.log("test1")
 
-    chrome.storage.local.set({ language: chosen }).then(() => {
-        console.log("Language is set");
-    });
+    localStorage.setItem('currentLanguage', chosen);
+    console.log(localStorage.getItem('currentLanguage'));
 });
 
 function setDefaultOption() {
-    chrome.storage.local.get(['language'], function(data) {
         const dropdown = document.getElementById('subject');
-        const storedValue = data.language;
+        let storedValue = localStorage.getItem('currentLanguage');
 
         // Check if there's a stored value and set it as selected
         if (storedValue) {
             dropdown.value = storedValue;
         }
         else {
-            dropdown.value = "English"
+            dropdown.value = "English";
         }
-    });
 }
 
+setDefaultOption()
 
 

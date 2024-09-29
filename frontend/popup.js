dd48d5f5237a74c6575f3067ca22d9f7ec059231
lchin <l.chin@ufl.edu>
2024-09-29 02:49:56 -0400

    popup

--- frontend/popup.js
+++ frontend/popup.js
@@ -2,22 +2,27 @@ document.getElementById('subject').addEventListener('change', function() {
     let selectedElement = document.querySelector('#subject');
     let chosen = selectedElement.value;
 
-    localStorage.setItem('currentLanguage', chosen);
-    console.log(localStorage.getItem('currentLanguage'));
+    // localStorage.setItem('currentLanguage', chosen);
+    // console.log(localStorage.getItem('currentLanguage'));
+    chrome.storage.local.set({"currentLanguage": chosen})
+    chrome.storage.local.get("currentLanguage", function(result) {
+       console.log(result["currentLanguage"]);
+    });
 });
 
 function setDefaultOption() {
-        const dropdown = document.getElementById('subject');
-        let storedValue = localStorage.getItem('currentLanguage');
+    const dropdown = document.getElementById('subject');
 
+    // Retrieve the stored value from Chrome storage
+    chrome.storage.local.get("currentLanguage", function(result) {
         // Check if there's a stored value and set it as selected
-        if (storedValue) {
-            dropdown.value = storedValue;
+        if (result.currentLanguage) {
+            dropdown.value = result.currentLanguage;
+        } else {
+            dropdown.value = "English"; // Default option
         }
-        else {
-            dropdown.value = "English";
-        }
-        console.log(localStorage.getItem("currentLanguage"));
+        console.log('Current language:', result.currentLanguage);
+    });
 }
 
 setDefaultOption()
